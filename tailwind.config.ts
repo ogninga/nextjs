eea1ea5a4cdaa7c5a08eb1316525a30787549d29
Your Name ogningakclearogningarun <neb.arts@gmail.com>
2024-01-12 19:36:29 -0500

    fixed starting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,10 @@ const config: Config = {
     },
   },
 
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("tailwindcss/nesting"),
+  ],
 };
 export default config;
