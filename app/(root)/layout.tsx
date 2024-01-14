785dce8d5b45fc194994daf6912576d956092528
Your Name ogningakclearogningarun <neb.arts@gmail.com>
2024-01-14 00:49:06 -0500

    Finished sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
+import LeftSideBar from "@/components/shared/LeftSideBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className=" background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-        LeftSideBar
+        <LeftSideBar />
         <section className="max-md:pd-14 flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -15,11 +17,11 @@ const Navbar = () => {
           alt="DevLyon"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 max-sm:hidden dark:text-light-900">
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500"> Lyon</span>
         </p>
       </Link>
-      GlobalSearch
+      <GlobalSearch />
       <div className="flex-between gap-5">
         <Theme />
       </div>
@@ -37,7 +39,7 @@ const Navbar = () => {
           }}
         />
       </SignedIn>
-      MobileNavigation
+      <MobileNav />
     </nav>
   );
 };
