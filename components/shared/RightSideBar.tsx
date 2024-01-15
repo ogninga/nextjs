2ca6dd8dde535cc7bbab8c7a39d30da99bc9397d
Your Name ogningakclearogningarun <neb.arts@gmail.com>
2024-01-14 23:39:24 -0500

    put base code for right side bar. classes

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const RightSideBar = () => {
-  return <div>RightSideBar</div>;
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px]  flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden"></section>
+  );
 };
 
 export default RightSideBar;
